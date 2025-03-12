ca7e6cb0de7f7cb5f3c16f2136ad80f328a4badf
hci-du-ed <hci.7712.du@gmail.com>
2025-03-13 03:47:43 +0900

    commit

---
+++ 2025_03_13/EditText_Test/app/src/main/java/com/example/edittext_test/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.edittext_test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    //에디트텍스트 edittext
+    //버튼 button
+    //텍스트뷰  textView
+
+    private EditText eText;
+    private Button eButton;
+    private TextView eTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        eText = (EditText) findViewById(R.id.edittext);
+        eButton = (Button) findViewById(R.id.button);
+        eTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void onClicked(View view) {
+        String str = eText.getText().toString();
+        eTextView.setText(str);
+
+    }
+}
